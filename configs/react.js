f45aec3ce3eb4bb517a6ad7d21d5bb441a0bb44b
tom <t.bass0326@gmail.com>
2021-09-11 13:32:45 +0900

    Add jsx-a11y plugin

--- configs/react.js
+++ configs/react.js
@@ -8,6 +8,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   parserOptions: {
     sourceType: 'module',
